99cd7887b6b1a6d5af43dc8d827de1a9c12d784a
Sebastián Maciel <sebastianmaciel@outlook.com>
2020-12-25 23:27:23 -0300

    Ver arribos desde la tabla - OK

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,18 @@ router.get("/ver-arribos", async (req, res) => {
   res.render("ver-arribos", { vuelos });
 });
 
+router.get("/verArriboDesdeTabla/:codigoArribo", async (req, res) => {
+  const { codigoArribo } = req.params;
+
+  const vuelos = await helpers.arribos();
+
+  let vuelo = vuelos.find((vuelos) => vuelos.vuelo === codigoArribo.toUpperCase());
+
+  if (vuelo) {
+    return res.render("vuelo", { vuelo });
+  }
+
+  res.render("vuelo", { vuelo: undefined, codigo: codigoArribo.toUpperCase() });
+});
+
 module.exports = router;
